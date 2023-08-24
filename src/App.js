9b468fbe30792c484f0c6687e6790e4c2daaa768
Tarek Mia <dev.tarekmia@gmail.com>
2023-08-24 22:03:44 +0600

    nav, home, sidebar, services, contact, footer --light/dark

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
+import About from './Components/About';
+import Services from './Components/Services';
+import Home from './Components/Home';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Clients from './Components/Clients';
+import Projects from './Components/Projects';
+import Reviews from './Components/Reviews';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ParallaxProvider>
+        <header id='home'>
+          <Navbar />
+          <Home />
+        </header>
+        <section id='services' className='bg-slate-200 dark:bg-slate-900'>
+          <Services />
+        </section>
+        <section id='about' className='bg-slate-200 dark:bg-slate-900 py-8'>
+          <About />
+        </section>
+        <section id='clients' className='bg-slate-200 dark:bg-slate-900 py-8'>
+          <Clients />
+        </section>
+        <section id='projects' className='bg-slate-200 dark:bg-slate-900 py-8'>
+          <Projects />
+        </section>
+        <section id='testimonial' className='bg-slate-200 dark:bg-slate-900 py-8'>
+          <Reviews />
+        </section>
+        <section id='contact' className='bg-slate-200 dark:bg-slate-900 py-8'>
+          <Contact />
+        </section>
+        <Footer />
+      </ParallaxProvider>
+    </>
   );
 }
 
